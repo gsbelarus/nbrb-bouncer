318393c73a1b658f60633a4bb23e255c9c92ea4d
andreik <gs1994@gmail.com>
2022-10-11 15:34:40 +0200

    fix

--- bouncer.js
+++ bouncer.js
@@ -6,7 +6,7 @@ const requestListener = async function (req, res) {
 
   if (data.ok) {
     res.writeHead(200);
-    res.end(data.text());
+    res.end(await data.text());
   } else {
     res.writeHead(500);
     res.end(data.statusText);
